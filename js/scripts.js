3aaf2be9d7229b0414eb34efdbaa08570e3d205d
Brice FOLLEAS <brice.folleas@gmail.com>
2020-03-31 14:59:52 +0200

    need help

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //let template = document.querySelector("#listeVoyage");
-let template = document.getElementById("grid-template");
+let template = document.querySelector("#grid-template");
 
 //récupération des données du fichier JSON
 
@@ -16,12 +16,34 @@ fetch("http://127.0.0.1:5500/../json/data.json").then(function (resp) {
                 .replace(/{{price}}/g, d.price);			// leur valeur
 
             clone.firstElementChild.innerHTML = newContent
-            document.getElementById("grid-template").appendChild(clone);				// On ajoute le clone créé
+            document.getElementById("grid-container").appendChild(clone);				// On ajoute le clone créé
         }
     })
 })
 
 
+/*
 
+let template = document.querySelector("#listeVoyage");
 
+//récupération des données du fichier JSON
 
+let destination;
+
+fetch("http://127.0.0.1:5500/Ressources/destination.json").then(function (resp) {
+    resp.json().then(function (data) {
+        for (const d of data) {					// itère sur le tableau
+            let clone = document.importNode(template.content, true);      // clone le template
+
+            newContent = clone.firstElementChild.innerHTML
+                .replace(/{{image}}/g, d.image)		// remplace {{modèle}}
+                .replace(/{{ville}}/g, d.ville)				// et {{couleur}} par
+                .replace(/{{prix}}/g, d.prix);			// leur valeur
+
+            clone.firstElementChild.innerHTML = newContent
+            document.getElementById("conteneur").appendChild(clone);				// On ajoute le clone créé
+        }
+    })
+})
+
+*/
